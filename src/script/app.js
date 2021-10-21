291437e50c095df8c25499cc15d0d5ceba29566c
aleksei24 <lexa24032@gmail.com>
2021-10-21 17:58:14 +0300

    got random image from the api

--- src/script/app.js
+++ src/script/app.js
@@ -1,9 +1,12 @@
+const meals = document.querySelector('#meals');
+
 function getRandomMeal() {
     try {
         const resp = fetch('https://www.themealdb.com/api/json/v1/1/random.php').then((responce) =>
             responce.json().then((data) => {
                 const randomMeal = data.meals[0];
                 console.log(randomMeal);
+                addMeal(randomMeal, true);
             })
         );
     } catch (err) {
@@ -17,6 +20,23 @@ function getMealById(id) {
 
 function getMealsBySearch(term) {
     const meals = fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term);
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('article');
+    meal.classList = 'meal';
+    meal.innerHTML = `
+      <div class="meal__header" id="put-pic-four">
+        ${random ? `<span class="random">Random Recipe</span>` : ''}
+        <img src='${mealData.strMealThumb}' alt='${mealData.Meal}'
+      </div>
+      <div class="meal__body">
+        <h5>${mealData.strMeal}</h5>
+        <button type="button">Press</button>
+      </div>
+    `;
+
+    meals.appendChild(meal);
 }
 
 // ========================================
